3aae213e3c88dd19b52140a5b16426c70f6d5b42
Bárbara Barone <babi.barone@cora.com.br>
2024-06-19 17:36:50 -0300

    cria projeto e adiciona feeditem

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Cora on 19/06/24.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
